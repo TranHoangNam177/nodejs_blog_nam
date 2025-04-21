ffb84958d96964d373f98c6f1ccf26f77c247b8e
TranHoangNam177 <tranhoangnam1707@gmail.com>
2025-04-21 20:58:29 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000   
+
+// định nghĩa route 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+// localhost là 127.0.0.1 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
